bd70bcb011fd660ae1ecfe7746ae3f2d97b08a61
matheusfillipeag <matheusfillipeag@gmail.com>
2019-05-19 02:05:25 -0300

    Janela de Modalidades

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,8 +1,7 @@
 ''' GANEC = Gestor de Alunos Nas Escolas do Carmo '''
 
-from PyQt5 import QtWidgets, QtGui, uic
+from PyQt5 import QtWidgets, QtGui, uic, Qt
 from fbs_runtime.application_context import ApplicationContext
-
 
 import sys
 
@@ -11,9 +10,9 @@ MODALIDADES_DIALOD, _ = uic.loadUiType("./src/main/python/ui/dialogs/modalidades
 NEW_MODALIDADE_WIDGET, _ = uic.loadUiType("./src/main/python/ui/widgets/modalidadeForm.ui")
 
 class NewModalidadeWidget(QtWidgets.QWidget, NEW_MODALIDADE_WIDGET):
-    def __init(self):
+    def __init__(self):
         QtWidgets.QWidget.__init__(self)
-        NEW_MODALIDADE_WIDGET.__init(self)
+        NEW_MODALIDADE_WIDGET.__init__(self)
         self.setupUi(self)
 
 class MainWindow(QtWidgets.QMainWindow, MAIN_WINDOW):
@@ -25,36 +24,84 @@ class MainWindow(QtWidgets.QMainWindow, MAIN_WINDOW):
     
     def modalidadesDialog(self):
         dialog=ModalidadesDialog(self)
+        dialog.setModal(True)
         dialog.show()
-        dialog.exec()
+        dialog.exec_()
 
 class ModalidadesDialog(QtWidgets.QDialog, MODALIDADES_DIALOD):
     def __init__(self, iface):
         QtWidgets.QDialog.__init__(self)
         MODALIDADES_DIALOD.__init__(self)
         self.setupUi(self)
+        
+        #carregar da DB
         self.addToListWidget1()
+        self.addToListWidget2()
+        self.listWidget.itemClicked.connect(self.modalidadeChanged)
+
 
     def addToListWidget1(self):
         itemN = QtWidgets.QListWidgetItem() 
         widget = NewModalidadeWidget()
+        widget.label.setText("Nome da Modalidade")
         widget.pushButton.clicked.connect(self.addToListWidget1)
         widget.pushButton.clicked.connect(lambda: widget.pushButton.setText("Remover"))
-        widget.pushButton.clicked.connect(lambda: widget.pushButton.clicked.connect(self.removeToListWidget1))
+        widget.pushButton.clicked.connect(lambda: self.setRemovableFromListWidget1(widget, itemN))
+
         widget.horizontalLayout.addStretch()
-        widget.horizontalLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
+        widget.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
         itemN.setSizeHint(widget.sizeHint())    
         self.listWidget.addItem(itemN)
         self.listWidget.setItemWidget(itemN, widget)
 
-    def removeToListWidget1(self):
-        pass
+    def removeFromListWidget1(self, item):
+        self.listWidget.takeItem(self.listWidget.row(item))   
+
+    def setRemovableFromListWidget1(self, widget, itemN):
+        while True:
+            try: widget.pushButton.clicked.disconnect() 
+            except Exception: break
+        widget.lineEdit.setReadOnly(True)
+        widget.pushButton.clicked.connect(lambda: self.removeFromListWidget1(itemN))
+
+    def modalidadeChanged(self):
+        #item=self.listWidget.currentItem()
+        self.listWidget_2.clear()         
+        self.addToListWidget2()
+      
+    def addToListWidget2(self):
+        #load from DB
+        itemN = QtWidgets.QListWidgetItem() 
+        widget = NewModalidadeWidget()
+        widget.label.setText("Nome da Turma")
+        widget.pushButton.clicked.connect(self.addToListWidget2)
+        widget.pushButton.clicked.connect(lambda: widget.pushButton.setText("Remover"))
+        widget.pushButton.clicked.connect(lambda: self.setRemovableFromListWidget2(widget, itemN))
+
+        widget.horizontalLayout.addStretch()
+        widget.horizontalLayout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
+        itemN.setSizeHint(widget.sizeHint())    
+        self.listWidget_2.addItem(itemN)
+        self.listWidget_2.setItemWidget(itemN, widget)
+
+    def removeFromListWidget2(self, item):
+        return self.listWidget_2.takeItem(self.listWidget_2.row(item))
+   
+
+    def setRemovableFromListWidget2(self, widget, itemN):
+        while True:
+            try: widget.pushButton.clicked.disconnect() 
+            except Exception: break
+        widget.lineEdit.setReadOnly(True)
+        widget.pushButton.clicked.connect(lambda: self.removeFromListWidget2(itemN))
+
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     win=MainWindow()
-    win.show()    
-    sys.exit(app.exec())
+    win.showMaximized()        
+    sys.exit(app.exec_())
 
 
 

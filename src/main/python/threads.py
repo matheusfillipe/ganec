04855ec0129bca110f5b47d36a7536a3e431ec22
matheusfillipe <matheusfillipeag@gmail.com>
2019-10-08 14:24:12 -0300

    8 de outubro

--- src/main/python/threads.py
+++ src/main/python/threads.py
@@ -6,11 +6,10 @@ from data.aluno import Aluno
 
 
 from pathlib import Path
-import csv, time
-from PyQt5.QtWidgets import QFileDialog
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.QtCore import pyqtSignal
-
+import csv, time, sys, math
+from PyQt5.QtWidgets import QFileDialog, QWidget
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtCore import pyqtSignal, Qt, QTimer
 
 # Nomes dos atributos:
 IDADE='idade'
@@ -197,57 +196,122 @@ class calcularRotasThread(QtCore.QThread):
             dbA.update(aluno['id'],{'escola': listaDeAlunos[j]['escola']})
             #TODO imcrementar turmas 
 
-                
-def test():
+class Runner(QtCore.QThread):
     
-    import math, sys
-    from PyQt5.QtCore import Qt, QTimer
-    from PyQt5 import QtWidgets, QtGui
-    from PyQt5.QtWidgets import QWidget
+    def __init__(self, target, *args, **kwargs):
+        super().__init__()
+        self._target = target
+        self._args = args
+        self._kwargs = kwargs
 
-    class Overlay(QWidget):
-        def __init__(self, parent = None):    
-            QWidget.__init__(self, parent)
-            palette = QtGui.QPalette(self.palette())
-            palette.setColor(palette.Background, Qt.transparent)
-            self.setPalette(palette)
-        
-        def paintEvent(self, event):
-        
-            painter = QtGui.QPainter()
-            painter.begin(self)
-            painter.setRenderHint(QtGui.QPainter.Antialiasing)
-            painter.fillRect(event.rect(), QtGui.QBrush(QtGui.QColor(255, 255, 255, 127)))
-            painter.setPen(QtGui.QPen(Qt.NoPen))
-            
-            for i in range(6):
-                if (self.counter / 5) % 6 == i:
-                    painter.setBrush(QtGui.QBrush(QtGui.QColor(127 + (self.counter % 5)*32, 127, 127)))
-                else:
-                    painter.setBrush(QtGui.QBrush(QtGui.QColor(127, 127, 127)))
-                painter.drawEllipse(
-                    self.width()/2 + 30 * math.cos(2 * math.pi * i / 6.0) - 10,
-                    self.height()/2 + 30 * math.sin(2 * math.pi * i / 6.0) - 10,
-                    20, 20)
-            
-            painter.end()
-        
-        def showEvent(self, event):
-        
-            self.timer = self.startTimer(50)
-            self.counter = 0
-        
-        def timerEvent(self, event):
-        
-            self.counter += 1
-            self.update()
-            if self.counter == 60:
-                self.killTimer(self.timer)
-                self.hide()
+    def run(self):
+        if len(self._kwargs.keys())==0 and len(self._args)==0:
+            self._target()   
+        else:
+            self._target(*self._args, **self._kwargs)
+
+def nogui(func):
+    from functools import wraps
+    @wraps(func)
+    def async_func(*args, **kwargs):
+        runner = Runner(func, *args, **kwargs)
+        # Keep the runner somewhere or it will be destroyed
+        func.__runner = runner
+        runner.start()
+
+    return async_func
+                
+
+class Overlay(QWidget):
+    stoped=pyqtSignal()
+    started=pyqtSignal()
+    def __init__(self, parent = None, text="", N=8): 
+        self.parent=parent
+        QWidget.__init__(self, parent)
+        parent: QtWidgets.QWidget
+        palette = QtGui.QPalette(self.palette())
+        palette.setColor(palette.Background, Qt.transparent)
+        self.setPalette(palette)
+        self.label=QtWidgets.QLabel(text, self)
+        self.label.setAlignment(Qt.AlignHCenter)
+        self.label.setFont(QtGui.QFont("Arial", 18, QtGui.QFont.Bold))
+        self.label.setStyleSheet('color: rgb(240,240,240); ')  #border: 2px solid gray;')
+        gf=QtWidgets.QGraphicsDropShadowEffect(self)
+        gf.setXOffset(2);
+        gf.setYOffset(10);
+        gf.setBlurRadius(8);
+        gf.setColor(QtGui.QColor(10, 10, 30, 200));
+        self.label.setGraphicsEffect(gf)
+        self.label.setWordWrap(True)
+        if parent is not None:
+             self.label.setFixedWidth(parent.width())
+                
+        self.hide()            
+        self.N=N
+        self.stoped.connect(self.stop)
+        self.started.connect(self.start)
 
 
+    def paintEvent(self, event):        
+        painter = QtGui.QPainter()
+        painter.begin(self)
+        painter.setRenderHint(QtGui.QPainter.Antialiasing)
+        painter.fillRect(event.rect(), QtGui.QBrush(QtGui.QColor(60, 60, 60, 200)))
+        painter.setPen(QtGui.QPen(Qt.NoPen))
+        N=self.N
+        R=max(35,min(self.width(),self.height())/8)
+        r=max(25, 2*3.1416*R/N-10)
+        d=5        
+        self.label.move(12, self.height()/2+R+r+12)
+        self.label.setFont(QtGui.QFont("Arial", int(max(min(18*self.height()/350, 35),10)), QtGui.QFont.Bold))
+
+        for i in range(N):
+            I=int(self.counter / 5) % N
+            if I == i:
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(100+d*3, 127+d*3, 240+d*3)))
+            elif i==I-1 or I-1+N==i:
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(100+d*2, 127+d*2, 220+d*2)))
+            elif i==I-2 or I-2+N==i:
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(100+d, 127+d, 200+d)))
+            elif i==I-3 or I-3+N==i:
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(100, 127, 150)))
+            else:
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(100, 100, 100, 250)))
+
+            painter.drawEllipse(
+                self.width()/2 + R * math.cos(2 * math.pi * i / N),
+                self.height()/2 + R * math.sin(2 * math.pi * i / N),
+                r, r)
+        painter.end()
+    
+    def showEvent(self, event):        
+        self.timer = self.startTimer(15)
+        self.counter = 0
+    
+    def timerEvent(self, event):        
+        self.counter += 1
+        self.update()
+
+#        if self.counter == 240:
+#            self.stop()
+    def start(self):
+        self.show()
+
+    def stop(self):
+        self.killTimer(self.timer)
+        self.hide()
+
+    def resize(self, w):
+        if self.parent is not None:
+            self.label.setFixedWidth(self.parent.width()-24)    
+        super().resize(w)
+
+
+
+
+     
+def test():
     class MainWindow(QtWidgets.QMainWindow):
-
         def __init__(self, parent = None):
         
             super(QtWidgets.QMainWindow, self).__init__(parent)        
@@ -256,19 +320,31 @@ def test():
             self.editor.setPlainText("0123456789"*100)
             layout = QtWidgets.QGridLayout(widget)
             layout.addWidget(self.editor, 0, 0, 1, 3)
-            button = QtWidgets.QPushButton("Wait")
-            layout.addWidget(button, 1, 1, 1, 1)
-            
+            button = QtWidgets.QPushButton("Calcular")
+            layout.addWidget(button, 1, 1, 1, 1)            
             self.setCentralWidget(widget)
-            self.overlay = Overlay(self.centralWidget())
-            self.overlay.hide()
-            button.clicked.connect(self.overlay.show)
-        
-        def resizeEvent(self, event):
-        
-            self.overlay.resize(event.size())
+
+            self.overlay = Overlay(self,"Calculando Absolutamente Nada...")  #Overlay de loading passando qual widget ele deve cobrir e um texto opicional para aparecer
+            #self.overlay = Overlay(self)
+            button.clicked.connect(self.compute) #O botão ativa a função para rodar em background
+       
+        def resizeEvent(self, event):        #todo widget tem esse método e essa é a melhor maneira de redimensionar o loading overlay quando o usuário der resize na janela
+            self.overlay.resize(event.size()) #redimensiona o overlay
             event.accept()
 
+        @nogui  #Isso aqui é um decorator. A função que ficar abaixo dele vai rodar em background quando chamada, ou seja, em outro thread
+        def compute(self, a=None):
+            #O qt só permite se comunicar com a main thread por sinais, por isso nunca modifique a UI diretamente por aqui (como criar widgets, mudar textos, etc...)
+            self.overlay.started.emit()              # Inicia o overlay emitindo um sinal (loading)
+
+            # Uma tarefa para rodar em segundo plano
+            import time 
+            for i in range(20):
+                time.sleep(.5)
+                print("Running....   ", i)
+            print("Over.")
+
+            self.overlay.stoped.emit() #para o overlay quando a tarefa estiver pronta
 
 
 
